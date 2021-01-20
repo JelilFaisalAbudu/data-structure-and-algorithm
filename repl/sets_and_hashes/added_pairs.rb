f677c185c7d1ac63a4a4f0519ed8be22b2c48fd5
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-01-20 21:48:46 +0000

    Fix rubocop errors

--- repl/sets_and_hashes/added_pairs.rb
+++ repl/sets_and_hashes/added_pairs.rb
@@ -1,15 +1,16 @@
 def find_added_pairs(array, k)
   pairs = []
   set = []
-  
+
   array.each do |element|
-    next if (set.include?(element))
+    next if set.include?(element)
+
     second_element = (k - element)
-    if array.include?(second_element)
-      pairs << [element, second_element]
-      set << element
-      set << second_element
-    end
+    next unless array.include?(second_element)
+
+    pairs << [element, second_element]
+    set << element
+    set << second_element
   end
   pairs
 end
--- repl/sets_and_hashes/spec/added_pairs_spec.rb
+++ repl/sets_and_hashes/spec/added_pairs_spec.rb
@@ -3,7 +3,7 @@ require_relative '../added_pairs.rb'
 describe 'Added Pairs' do
   context 'Given a list of numbers to find all pairs whose sum is k' do
     it 'returns [[1, 11], [9, 3]] from the list [1, 9, 11, 13, 2, 3, 7] whose sum is 12' do
-      expect(find_added_pairs([1, 9, 11, 13, 2, 3, 7], 12)).to eq([[1, 11], [9, 3]]) 
+      expect(find_added_pairs([1, 9, 11, 13, 2, 3, 7], 12)).to eq([[1, 11], [9, 3]])
     end
 
     it 'returns [[-8, 8], [9, -9]] from the list [1, 9, 11, 13, 2, 3, 7] whose sum is 0' do

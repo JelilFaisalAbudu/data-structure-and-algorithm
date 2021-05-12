4ea0dbb29a72bd1b656b43c9180773f0d5b9854e
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-05-12 06:07:10 +0000

    Define a method to add data at front of the linked list

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -1,7 +1,33 @@
 class Node
+  attr_accessor :data, :next_node
 
+  def initialize(data, next_node = nil)
+    @data = data
+    @next_node = next_node
+  end 
 end
 
 class LinkedList
-  
+  attr_accessor :head, :next
+
+  def initialize
+    @head = nil
+    @next = nil
+  end
+
+  def add_at_front(data)
+    new_node = Node.new(data)
+    if @head.nil?
+      @head = new_node
+    else
+      new_node.next_node = @head
+      @head = new_node
+    end
+  end
 end
+
+linked_list = LinkedList.new
+linked_list.add_at_front(23)
+puts linked_list.head.data
+linked_list.add_at_front(2)
+puts linked_list.head.data

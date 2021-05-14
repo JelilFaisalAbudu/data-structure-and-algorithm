a91cda70c1541dc63b9052bb39b331f72067337e
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-05-14 05:33:52 +0000

    Define the method to remove edges between vertices

--- graphs/undirected-graph.rb
+++ graphs/undirected-graph.rb
@@ -23,13 +23,42 @@ class UndirectedGraph
     @edges[vertex1][vertex2] = weight
     @edges[vertex2][vertex1] = weight
   end
+
+  def remove_edge(vertex1, vertex2)
+    if @edges[vertex1] && @edges[vertex1][vertex2]
+      @edges.delete(@edges[vertex1][vertex2])
+    end
+
+    if @edges[vertex2] && @edges[vertex2][vertex1]
+      @edges.delete(@edges[vertex2][vertex1])
+    end
+  end
 end
 
-graph1 = UndirectedGraph.new
-graph1.add_vertex(1)
-graph1.add_vertex(2)
+# graph1 = UndirectedGraph.new
+# graph1.add_vertex(1)
+# graph1.add_vertex(2)
 
-puts graph1.edges
+# puts graph1.edges
 
-graph1.add_edge(1, 2, 1)
-puts graph1.edges
+# graph1.add_edge(1, 2, 1)
+# puts graph1.edges
+
+puts 'Graph 2'
+graph2 = UndirectedGraph.new
+
+graph2.add_vertex(2)
+graph2.add_vertex(5)
+graph2.add_edge(2,5, 1)
+puts graph2.edges
+graph2.add_vertex(10)
+graph2.add_vertex(23)
+graph2.add_vertex(51)
+graph2.add_vertex(25)
+graph2.add_edge(5, 10, 2)
+graph2.add_edge(10, 23, 4)
+graph2.add_edge(23, 25, 2100)
+p graph2.edges
+
+graph2.remove_edge(10, 23)
+p graph2.edges

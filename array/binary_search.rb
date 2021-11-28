9a5777298fd5da73eb56e88ffbb17a9b12beb0d9
JelilAbuduFaisal <abudujelilfaisal@gmail.com>
2021-11-28 20:42:54 +0000

    Create a binary search script

---
+++ array/binary_search.rb
@@ -0,0 +1,16 @@
+def binary_search(array, element)
+  lower_bound = 0
+  upper_bound = array.length - 1
+ 
+  while lower_bound <= upper_bound
+    mid_bound = (lower_bound + upper_bound) / 2
+    if element == array[mid_bound]
+      return mid_bound
+    elsif element < array[mid_bound]
+      upper_bound = mid_bound - 1
+    else
+      lower_bound = mid_bound + 1
+    end
+  end
+  -1
+end

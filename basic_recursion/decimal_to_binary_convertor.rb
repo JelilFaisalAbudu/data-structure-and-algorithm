fdbbcba9358ef0f755186ed700c5a1b120b7ed73
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-12-05 23:24:08 +0000

    create a decimal-to-binary convertor barebone method

--- basic_recursion/decimal_to_binary_convertor.rb
+++ basic_recursion/decimal_to_binary_convertor.rb
@@ -1,13 +1,14 @@
-def convert_decimal_to_binary(num)
-  binary_string = ''
-
+def convert_decimal_to_binary(num, binary_string = '')
+  puts binary_string
   if num < 2
     binary_string += (num % 2).to_s
+    puts binary_string
+    binary_string
   else
     convert_decimal_to_binary(num / 2)
-    binary_string += (num % 2).to_s
+   binary_string += (num % 2).to_s
   end
   binary_string
 end
 
- puts convert_decimal_to_binary(232)
+ puts convert_decimal_to_binary(2)

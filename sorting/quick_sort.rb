cb48c8a2c22450510608aacde974acfea3904aaa
JelilAbuduFaisal <abudujelilfaisal@gmail.com>
2021-12-10 22:04:01 +0000

    create a quick sort method

---
+++ sorting/quick_sort.rb
@@ -0,0 +1,20 @@
+def quick_sort(array)
+  cloned_array = array.clone
+  for outer_index in 1...array.length do
+    inner_index = outer_index
+    
+    while inner_index > 1 && cloned_array[inner_index - 1] > cloned_array[inner_index]
+      swap(cloned_array, inner_index - 1, inner_index)
+      inner_index = inner_index - 1
+    end
+    p cloned_array
+  end
+  
+  cloned_array
+end
+
+def swap(array, first_index, second_index)
+  temp = array[first_index]
+  array[first_index] = array[second_index]
+  array[second_index] = temp
+end

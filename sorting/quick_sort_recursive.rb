6d7b44e27792932ba3ae901409d5fce432fc20c2
JelilAbuduFaisal <abudujelilfaisal@gmail.com>
2021-12-12 21:14:11 +0000

    Complete the final quick_sort_recursive method

--- sorting/quick_sort_recursive.rb
+++ sorting/quick_sort_recursive.rb
@@ -1,3 +1,15 @@
+def quick_sort_recusive(array, low_index = 0, high_index = array.length - 1)
+  
+  if low_index < high_index
+    pivot = partition(array, low_index, high_index)
+    
+    quick_sort_recusive(array, low_index, pivot - 1)
+    quick_sort_recusive(array, pivot + 1, high_index)
+  end
+  
+  array
+end
+
 def partition(array, low = 0, hi = array.length - 1)
   pivot = array[low]
   i = low + 1
